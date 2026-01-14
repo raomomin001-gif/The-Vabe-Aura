2501bf9d46de8959bee4dc2d9af1a5b734d073bc
khantk76988-alt <khantk76988@gmail.com>
2026-01-14 16:43:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,25 @@
 function openWhatsApp(){
   window.open("https://wa.me/923042249321","_blank");
 }
+let slideIndex = 0;
+showSlides();
+
+function showSlides(){
+  let slides = document.getElementsByClassName("slides");
+
+  for(let i=0; i<slides.length; i++){
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if(slideIndex > slides.length){
+    slideIndex = 1;
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // 3 seconds
+}
+
+function openWhatsApp(){
+  window.open("https://wa.me/923042249321","_blank");
+}
