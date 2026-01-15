4fb9fa54e1ead022ee4ba133eda59fe6bd9723ca
khantk76988-alt <khantk76988@gmail.com>
2026-01-15 12:17:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -24,20 +24,3 @@ function showSlides(){
 function openWhatsApp(){
   window.open("https://wa.me/923042249321","_blank");
 }
-let slideIndex = 0;
-showSlides();
-
-function showSlides(){
-  let slides = document.querySelectorAll(".slides");
-
-  slides.forEach(slide => slide.style.display = "none");
-
-  slideIndex++;
-  if(slideIndex > slides.length){
-    slideIndex = 1;
-  }
-
-  slides[slideIndex - 1].style.display = "block";
-
-  setTimeout(showSlides, 3000); // 3 second
-}
