3db766d6d6ffe74e59e446f7ba2c559019553b15
khantk76988-alt <khantk76988@gmail.com>
2026-01-18 18:12:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
 function toggleMenu(){
-  document.getElementById("menu").classList.toggle("active");
+document.getElementById("menu").classList.toggle("active");
 }
 
-let index=0;
-function showSlides(){
-  const slides=document.querySelectorAll(".slides");
-  slides.forEach(s=>s.style.display="none");
-  index++;
-  if(index>slides.length){index=1}
-  slides[index-1].style.display="block";
-  setTimeout(showSlides,3000);
-}
-
-function acceptAge(){
-  document.getElementById("agePopup").classList.add("hide");
-  localStorage.setItem("age","yes");
-}
-
-function openWhatsApp(){
-  window.open(
-    "https://wa.me/923042249321?text=Hello%20I%20want%20to%20order%20from%20The%20Vape%20Aura",
-    "_blank"
-  );
+function openWhatsApp(product){
+window.open(
+`https://wa.me/923042249321?text=Hi%20I%20want%20to%20order%20${product}`,
+"_blank"
+);
 }
 
 function callNow(){
-  window.location.href="tel:03042249321";
+window.location.href="tel:+923042249321";
 }
 
-document.addEventListener("DOMContentLoaded",()=>{
-  if(localStorage.getItem("age")==="yes"){
-    document.getElementById("agePopup").classList.add("hide");
-  }
-  showSlides();
-});
+function acceptAge(){
+document.getElementById("agePopup").style.display="none";
+}
+
+function denyAge(){
+window.location.href="https://google.com";
+}
+
+/* SLIDER */
+let index=0;
+const slides=document.querySelectorAll(".slide");
+setInterval(()=>{
+slides.forEach(s=>s.classList.remove("active"));
+index=(index+1)%slides.length;
+slides[index].classList.add("active");
+},3000);
